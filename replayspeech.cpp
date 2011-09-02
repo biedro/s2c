98b97d369587d5ce80f96d0fe2f5f763d734cece
biedro <biedro@o2.pl>
2011-09-02 16:15:16 +0200

    Adding new classes for future reference
    ?

---
+++ replayspeech.cpp
@@ -0,0 +1,14 @@
+#include "replayspeech.h"
+
+//String tts = "http://translate.google.com/translate_tts?tl=fr&q=belle&ie=UTF-8";
+//tts = "http://translate.google.com/translate_tts?ie=UTF-8&q=bonjour&tl=fr";
+//SoundController sdctr = new SoundController();
+//sdctr.setPreferredSoundType(Html5Sound.class);
+//sdctr.setGwtVoicesSwfLocation(GWT.getHostPageBaseURL() + "" + GWT.getModuleName() + "/");
+//Sound sound = sdctr.createSound(Sound.MIME_TYPE_AUDIO_MPEG_MP3,tts);
+//sound.play();
+
+
+ReplaySpeech::ReplaySpeech()
+{
+}
---
+++ replayspeech.h
@@ -0,0 +1,10 @@
+#ifndef REPLAYSPEECH_H
+#define REPLAYSPEECH_H
+
+class ReplaySpeech
+{
+public:
+    ReplaySpeech();
+};
+
+#endif // REPLAYSPEECH_H
